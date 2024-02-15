a2f000c11cb72033c69dfdbc8b4035f4ff0ae982
Maxine Aubrey <max@ine.dev>
2024-02-15 23:17:55 +0100

    gnome.mutter: 45.4 → 46.beta
    https://gitlab.gnome.org/GNOME/mutter/-/compare/45.4...46.beta

--- pkgs/desktops/gnome/core/mutter/default.nix
+++ pkgs/desktops/gnome/core/mutter/default.nix
@@ -67,13 +67,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "mutter";
-  version = "45.4";
+  version = "46.beta";
 
-  outputs = [ "out" "dev" "man" "devdoc" ];
+  outputs = [ "out" "dev" "man" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/mutter/${lib.versions.major finalAttrs.version}/mutter-${finalAttrs.version}.tar.xz";
-    sha256 = "kRQIN74VWC8sdTvmYauOQtrVXUobDwZQvQssk/Ar16s=";
+    sha256 = "sha256-xGc6apHrVCaFwCY3FlWaMbHgu98bQC8adSf//w044TM=";
   };
 
   mesonFlags = [
