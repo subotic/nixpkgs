2caca47391d4fea9f28af95692d113323a0512c4
Maxine Aubrey <max@ine.dev>
2024-02-15 20:18:08 +0100

    gjs: 1.78.4 → 1.79.3
    https://gitlab.gnome.org/GNOME/gjs/-/compare/1.78.4...1.79.3

--- pkgs/development/libraries/gjs/default.nix
+++ pkgs/development/libraries/gjs/default.nix
@@ -32,13 +32,13 @@ let
   ];
 in stdenv.mkDerivation (finalAttrs: {
   pname = "gjs";
-  version = "1.78.4";
+  version = "1.79.3";
 
   outputs = [ "out" "dev" "installedTests" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/gjs/${lib.versions.majorMinor finalAttrs.version}/gjs-${finalAttrs.version}.tar.xz";
-    hash = "sha256-mux6uHLCBQQEkHrpTwrnq+yKVL2ciU3bXC0PUekyuaE=";
+    hash = "sha256-OzM+17KCvKbyPVKNr3MDcwhSR3QPLFGh1w/a8GnNYzU=";
   };
 
   patches = [
